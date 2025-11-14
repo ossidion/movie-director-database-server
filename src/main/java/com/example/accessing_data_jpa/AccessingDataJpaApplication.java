c9fe82fe750c1cebd27345233e597c49f1a3a48a
ossidion <alexinbristol@outlook.com>
2025-11-14 10:31:32 +0000

    update application main

--- src/main/java/com/example/accessing_data_jpa/AccessingDataJpaApplication.java
+++ src/main/java/com/example/accessing_data_jpa/AccessingDataJpaApplication.java
@@ -1,28 +1,43 @@
 package com.example.accessing_data_jpa;
 
-import com.example.accessing_data_jpa.services.ReadData;
+import com.example.accessing_data_jpa.services.ReadCrewData;
+import com.example.accessing_data_jpa.services.ReadMovieData;
+import com.example.accessing_data_jpa.services.ReadPersonData;
+import com.example.accessing_data_jpa.services.BuildMovieDirectorTable;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
-// Console-entry point version of app (no web interface yet).
-
-// NOTE on CONTROLLER - can do this later for web interface.
-// @Controller = for HTML views (Thymeleaf, etc.).
-//@RestController = for REST APIs (return JSON).
-
 @SpringBootApplication
+@EntityScan("com.example.accessing_data_jpa.entities")
 public class AccessingDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataJpaApplication.class, args);
 	}
 
+	// The Application Context manages beans i.e. @Services, @Components, etc.
+
+	// @Bean - it runs automatically after the application context has started and right before the app is ready to serve http requests.
 	@Bean
-	public CommandLineRunner imdb(ReadData readData) {
+	public CommandLineRunner imdb(ReadMovieData readData,
+								  ReadCrewData crewData,
+								  ReadPersonData personData,
+								  BuildMovieDirectorTable buildMovieDirectorTable) {
+		boolean loadData = false;
 		return (args) -> {
-			readData.loadDataFromFile();
+			if (loadData) {
+				readData.loadDataFromFile();
+				crewData.loadDataFromFile();
+				personData.loadDataFromFile();
+				buildMovieDirectorTable.buildLinks();
+			}
+
 		};
 	}
 }
+
+
+// Return a lambda returning a new CommandLineRunner with the run method executes @Services (provides the code to run after Spring finishes initialization).
